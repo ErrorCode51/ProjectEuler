2a9490888e8dcdc6177fcc3f5bac62e83baa3511
ErrorCode51 <43345236+ErrorCode51@users.noreply.github.com>
2020-02-22 14:19:56 +0100

    Added problem 81 and improved problem 82

--- Euler_82.py
+++ Euler_82.py
@@ -4,18 +4,19 @@ with open('ProblemFiles/Problem_82.txt') as file:
     for line in file:
         sourceMatrix.append([int(n) for n in line.split(',')])
 
-sourceMatrix = [[line[i] for line in sourceMatrix] for i in range(80)]
+matrixSize = len(sourceMatrix[0])
+sourceMatrix = [[line[i] for line in sourceMatrix] for i in range(matrixSize)]
 
 cheapestMatrix = []
 cheapestMatrix.append(sourceMatrix[0])
 
 
-for x in range(1, 80):
+for x in range(1, matrixSize):
     cheapestMatrix.append(sourceMatrix[x].copy())
-    for y in range(80):
+    for y in range(matrixSize):
         cheapestMatrix[x][y] += cheapestMatrix[x-1][y]
-    for y in range(80):
-        for y2 in range(80):
+    for y in range(matrixSize):
+        for y2 in range(matrixSize):
             if y2 < y:
                 if (sum(sourceMatrix[x][y2:(y+1)]) + cheapestMatrix[x-1][y2]) < cheapestMatrix[x][y]:
                     cheapestMatrix[x][y] = (sum(sourceMatrix[x][y2:(y+1)]) + cheapestMatrix[x-1][y2])
